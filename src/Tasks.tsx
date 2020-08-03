7bfbbda46771b46dae0bbdb693c8caf85b9ef696
Zhark10 <verygoodman18@yandex.ru>
2020-08-03 17:42:14 +0300

    task updated

--- src/Tasks.tsx
+++ src/Tasks.tsx
@@ -1,47 +1,79 @@
 import React from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
   Dimensions,
   TextInput,
+  Button,
 } from 'react-native';
 
 export interface ITask {
-  name: string,
-  time: number,
+  name: string;
+  time: number;
 }
 
 const Tasks = () => {
-  const [task, setTask] = React.useState({})
+  const [tasks, updatedTasksList] = React.useState<ITask[]>([]);
+  const [task, setTask] = React.useState<ITask>({
+    name: '',
+    time: 0,
+  });
 
   const onChangeText = (text: string) => {
-    console.log(text)
-  }
+    setTask({name: text, time: 0});
+  };
+
+  const createNewTask = () => {
+    updatedTasksList((currentList) => [...currentList, task]);
+  };
 
   return (
-    <View style={{
-      height: 1/4 * Dimensions.get('screen').height,
-      width: '100%',
-    }}>
-      <TextInput
-        autoFocus
-        style={{}}
-        onChangeText={onChangeText}
-        // onEndEditing={onEndEditing}
-      />
+    <View>
+      <View
+        style={{
+          height: (1 / 4) * Dimensions.get('screen').height,
+          width: '100%',
+          padding: 16,
+        }}>
+        <TextInput
+          autoFocus
+          placeholder="Название таска"
+          defaultValue=""
+          style={{
+            padding: 8,
+            borderWidth: 2,
+            borderRadius: 50,
+            borderColor: 'rgba(0,0,0,.54)',
+          }}
+          onChangeText={onChangeText}
+        />
+        <Button onPress={createNewTask} title="новый task" />
+      </View>
+      <View>
+        {tasks.map((elem, key) => (
+          <View
+            key={key}
+            style={{
+              width: '100%',
+              padding: 16,
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}>
+            <View>
+              <Text>Task: {elem.name}</Text>
+              <Text>Time: {elem.time}</Text>
+            </View>
+            <Text style={{backgroundColor: 'red', borderRadius: 40, height: 32, width: 32, textAlign: 'center', textAlignVertical: 'center', color: '#fff'}}>X</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    
-  },
-  
+  scrollView: {},
 });
 
 export default Tasks;
